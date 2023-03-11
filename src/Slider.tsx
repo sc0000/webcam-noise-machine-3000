54ca199a24e97cbe1268266c74d2f8e88c446001
sc0000 <sebastiancyliax@gmx.net>
2023-03-11 15:43:24 +0100

    consolidated useCallbacks in Slider

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -20,16 +20,18 @@ const Slider: FC<Slider> = ({id, mapping, activeUIElement, sendActiveUIElementTo
     let innerRef = useRef<HTMLDivElement>(null);
     let handleRef = useRef<HTMLDivElement>(null);
 
-    const width = useCallback(() => {
+    const sizeAndBoundaries = useCallback(() => {
       const left = innerRef.current?.getBoundingClientRect().left!;
       const right = innerRef.current?.getBoundingClientRect().right!;
-      const w = right - left + 1;
-      console.log(w);
-      return w;
-    }, [innerRef]);
+      const width = right - left + 1;
+      // height...
 
-    const left = useCallback(() => {
-      return innerRef.current?.getBoundingClientRect().left!;
+      return {
+        left: left,
+        right: right,
+        width: width,
+      }
+     
     }, [innerRef]);
 
     const [handlePosition, setHandlePosition] = useState(15);
@@ -38,47 +40,46 @@ const Slider: FC<Slider> = ({id, mapping, activeUIElement, sendActiveUIElementTo
 
     useEffect(() => {
       if (activeUIElement === id) {
-        const handleX = mouseX - left();
-        if (handleX > 0 && handleX < width()) {
+        const {left, width} = sizeAndBoundaries();
+
+        const handleX = mouseX - left;
+        if (handleX > 0 && handleX < width) {
           setHandlePosition(handleX);
-          console.log(width());
+          console.log(width);
         } else if (handleX < 0) {
           setHandlePosition(0);
-        } else setHandlePosition(width());
+        } else setHandlePosition(width);
 
         console.log(`handlePos: ${handlePosition}\nhandleX: ${handleX}`);
       }
     }, [mouseX]);
 
     const handleDown = (e: MouseEvent<HTMLElement>) => {
-          const x = e.clientX - left();
+          const x = e.clientX - sizeAndBoundaries().left;
           setHandlePosition(x);
           sendActiveUIElementToParent(id);
         }
 
     // TODO: Consolidate with context useEffect!
     useEffect(() => {
-          //
-          // Audio parameter mapping
-          //  
+      //
+      // Audio parameter mapping
+      //  
 
-          const left = innerRef.current?.getBoundingClientRect().left!;
-          const right = innerRef.current?.getBoundingClientRect().right!;
+      const {left, right} = sizeAndBoundaries();
         
-          // if (handlePosition > right) setHandlePosition(handlePosition - 15);
-          
-        if (mapping === 'playerVolume' && audio.players[id]) {
-          // const vol = scale(handlePosition, [0, right - left], [-12, 12]);
-          const vol = mapLinearToLogarithmicScale(handlePosition, 0, right - left, -12, 12);
-          console.log(`vol: ${vol}`);
-          audio.players[id!].volume.value = vol;
-        }
+      if (mapping === 'playerVolume' && audio.players[id]) {
+        // const vol = scale(handlePosition, [0, right - left], [-12, 12]);
+        const vol = mapLinearToLogarithmicScale(handlePosition, 0, right - left, -12, 12);
+        console.log(`vol: ${vol}`);
+        audio.players[id!].volume.value = vol;
+      }
 
-        if (mapping === 'microtonalSpread') {
-            // TODO: Get the log scaling right!
-            audio.microtonalSpread = 1000 - mapLinearToLogarithmicScale(handlePosition, 0, right - left, 0.1, 1000);
-            // console.log(audio.microtonalSpread);
-        }
+      if (mapping === 'microtonalSpread') {
+          // TODO: Get the log scaling right!
+          audio.microtonalSpread = 1000 - mapLinearToLogarithmicScale(handlePosition, 0, right - left, 0.1, 1000);
+          // console.log(audio.microtonalSpread);
+      }
     }, [handlePosition]);
 
   return (
